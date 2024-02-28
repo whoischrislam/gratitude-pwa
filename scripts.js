014d8ea5732cdddb9463b3d71f77381a3f887554
whoischrislam <whoischrislam@gmail.com>
2024-02-28 10:55:50 -1000

    Connected Firebase via JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://gratitude-pwa-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const gratitudeDB = ref(db, "gratitude");
+
